c8361dbbfff9e949f16d9b60d25a9105b125008f
User <prrvchr@gmail.com>
2019-09-27 13:15:26 +0200

    first commit

---
+++ make_package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#./make_rdb.sh
+
+cd ./CloudContactOOo/
+
+zip -0 CloudContactOOo.zip mimetype
+
+zip -r CloudContactOOo.zip *
+
+cd ..
+
+mv ./CloudContactOOo/CloudContactOOo.zip ./CloudContactOOo.oxt
